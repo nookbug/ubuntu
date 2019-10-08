20448c53edd7b3b84d26f1ab2bffc2b8cf07e84a
nookbug <56292314+nookbug@users.noreply.github.com>
2019-10-08 14:29:31 +0630

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get install pv -y
+clear
+
+sudo apt-get install vnc4server -y 
+sudo apt-get install xfce4 xfce4-goodies -y
+clear
+echo -e "Password For VNC Server" | pv -qL 120 
+sleep 3s 
+vncpasswd
+vncserver -geometry1366x678
+clear
+
+sudo apt-get install wget -y
+sudo apt-get install firefox -y
+clear
+
+sudo apt-get update -y
+clear
+exit
