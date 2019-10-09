b91a946b7c6676ffab423bcb8c8f1b2fcaa29555
nookbug <56292314+nookbug@users.noreply.github.com>
2019-10-09 09:59:24 +0630

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
 sudo apt-get install unzip zip -y
 clear
 sudo apt-get install wget -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+unzip *.zip
+clear
+ls 
+cd ngrok-stable-linux-386
+chmod +x ngrok
+./ngrok authtoken 2iv4LMsKSUsE4LVccZSjK_4vg5TYQ2UupWNA9qetpjq
+exit
