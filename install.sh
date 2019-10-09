8ada03b91d7d96afc866f87a24d5758b0fd0a400
nookbug <56292314+nookbug@users.noreply.github.com>
2019-10-09 10:26:52 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 sudo apt-get install pv -y
+sudo apt-get install figlet -y
 clear
 
 sudo apt-get install vnc4server -y 
 sudo apt-get install xfce4 xfce4-goodies -y
 clear
-echo -e "Password For VNC Server" | pv -qL 120 
+figlet "Password For VNC Server" | pv -qL 120 
 sleep 3s 
 vncpasswd
 vncserver -geometry1366x678
