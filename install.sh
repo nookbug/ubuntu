2783f0a817196c5a98d57ec4bc41003e8e42d386
nookbug <56292314+nookbug@users.noreply.github.com>
2019-10-16 12:55:28 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,19 @@
 sudo apt-get install pv -y
 sudo apt-get install figlet -y
 clear
-
-sudo apt-get install vnc4server -y 
-sudo apt-get install xfce4 xfce4-goodies -y
-clear
-figlet "Password For VNC Server" | pv -qL 120 
-sleep 3s 
-vncpasswd
-vncserver -geometry1366x678
-clear
-
 sudo apt-get install wget -y
 sudo apt-get install firefox -y
 clear
 
-sudo apt-get update -y
+sudo apt -y install vnc4server
+sudo apt-get install lxde lxde-core
 clear
+figlet "Password For VNC Server" | pv -qL 120 
+sleep 3s 
+vncpasswd
+
+vncserver -geometry 1024x768
+
+bash zip.sh
+
 exit
