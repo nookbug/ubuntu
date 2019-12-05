cd3c40b5c47fd7ad033355eddbb115ecc9c38bc7
nookbug <56292314+nookbug@users.noreply.github.com>
2019-12-05 19:33:22 +0630

    Create LXDE.sh

---
+++ LXDE.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+# Install X, LXDE, VPN programs
+
+apt-get install xorg lxde-core tightvncserver
+
+# Start VNC to create config file
+
+tightvncserver :1
+
+# Then stop VNC
+
+tightvncserver -kill :1
+
+# Edit config file to start session with LXDE:
+
+nano ~/.vnc/xstartup
+
+# Add this at the bottom of the file:
+lxterminal &
+/usr/bin/lxsession -s LXDE &
+
+# Restart VNC
+
+tightvncserver :1
