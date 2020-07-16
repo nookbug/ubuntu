ed6cfa8bf72cd8cbde8af4867edd3544a03dd52a
nookbug <56292314+nookbug@users.noreply.github.com>
2020-07-17 01:59:05 +0630

    Update LXDE.sh

--- LXDE.sh
+++ LXDE.sh
@@ -19,3 +19,15 @@ echo "lxterminal &
 # Restart VNC
 
 tightvncserver :1
+clear 
+echo -e 'Installing ....'
+
+sudo apt-get install unzip zip -y
+clear
+sudo apt-get install wget -y
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+unzip *.zip
+clear
+chmod +x ngrok
+./ngrok authtoken 2iv4LMsKSUsE4LVccZSjK_4vg5TYQ2UupWNA9qetpjq
+echo ' '
